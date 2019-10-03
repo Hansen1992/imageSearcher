e74b0187a361ae5ae002c096c2345c0fb8b21a4b
Martin Nøhr Åskov Hansen <50966456+Hansen1992@users.noreply.github.com>
2019-10-03 21:40:39 +0200

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,9 +7,7 @@ class SearchBar extends React.Component {
         e.preventDefault();
         this.props.onSubmit(this.state.term);
     };
-    //onInputChange(e) { onInputChange eller lign. er en standard måde at kalde dine functions, der håndterer eventhandlers, du vil derfor ofte se onInputChange, onInputClick og onInputSubmit eller et 'handle' foran istedet for 'on'
-        //e.target.value;
-    //}
+
     render() {
         return (
         <div className='ui segment'>
@@ -24,5 +22,4 @@ class SearchBar extends React.Component {
     }
 }
 
-// det er meget vigtigt at forstå hvornår du skal bruge this.onInputChange() og this.onInputChange. Hvis du tilføjer () så vil functionen blive kaldt lige så snart, at app'en render, mens hvis du ikke gør det, så er det først, ved onClick eller onchange eller onSubmit i dit input field, at der sker noget. Meget vigtig forskel og common error.
 export default SearchBar;
