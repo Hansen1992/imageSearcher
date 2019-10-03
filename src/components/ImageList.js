ed3c8d3c0b17cc19ecd62f9a09712161f0c66587
Martin Nøhr Åskov Hansen <50966456+Hansen1992@users.noreply.github.com>
2019-10-03 21:39:14 +0200

    Update ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,7 @@ import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <ImageCard key={image.id} image={image}/> //key prop er altid godt at bruge til når du skal fremkalde en liste af items, da den sørger for at identificere billederne hver især med et id. Key'en skal altid assignes root-elementet, så hvis <img> var inde i en div, så var det div'en der skulle have key proppen. 
+        return <ImageCard key={image.id} image={image}/>
     });
     return <div className="image-list">{images}</div>
 };
